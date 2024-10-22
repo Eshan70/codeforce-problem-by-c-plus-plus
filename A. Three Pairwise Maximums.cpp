5fc5e613cce40ca73cabe354ff34827e74a266f1
Eshan Munshi <122246307+Eshan70@users.noreply.github.com>
2024-10-22 12:42:05 +0600

    Add files via upload

---
+++ A. Three Pairwise Maximums.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+int t;
+cin>>t;
+
+while(t--){
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+
+        sort(a,a+3);
+        if(a[1]==a[2])
+        cout<<"YES"<<endl<<a[2]<<" "<<a[0]<<" "<<a[0]<<endl;
+        else
+         cout<<"NO"<<endl;
+	}
+
+	return 0;
+}
