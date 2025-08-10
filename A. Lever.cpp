d6e98eda15f5d7ccbca5766e3ea0f863de6ceade
Eshan Munshi <122246307+Eshan70@users.noreply.github.com>
2025-08-10 22:52:24 +0600

    Add files via upload

---
+++ A. Lever.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; i++)
+                cin >> a[i];
+        for (int i = 0; i < n; i++)
+         cin >> b[i];
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > b[i])
+                total += (a[i] - b[i]);
+        }
+
+        cout << total+ 1 << "\n";
+}}
